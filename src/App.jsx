b86227b449d29ce394ecaa8930e81c56d4866835
Pritam Das <dpritam28577@gmail.com>
2023-09-08 01:37:38 +0530

    add the react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css'
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import "./App.css";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </h1>
-  )
+  );
 }
