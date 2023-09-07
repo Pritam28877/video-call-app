6ba9d0f1032eb3f2d8858aaa8dd111a3c174364d
Pritam Das <dpritam28577@gmail.com>
2023-09-08 01:52:24 +0530

    add the Room component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
+import Room from "./components/Room";
 
 export default function App() {
   return (
     <h1 className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/room/:roomID" element={<Room />} />
       </Routes>
     </h1>
   );
