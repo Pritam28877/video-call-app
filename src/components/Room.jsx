b94caa175a560060565bf107c11ea6d5f3ece074
Pritam Das <dpritam28577@gmail.com>
2023-09-16 01:25:59 +0530

    add the generateKitTokenForTest config

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,7 +1,27 @@
 import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 const Room = () => {
   const { roomID } = useParams();
   console.log(roomID);
+  const meeting = async (element) => {
+    const appID = 189493556;
+    const serverSecret = "5f7664356598c2f13f3339cdead04d83";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Guset"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
   return <div>Room</div>;
 };
 
